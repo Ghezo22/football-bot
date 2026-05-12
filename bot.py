aa561d3cc0f42bca2fd44db8e98015d2bcb0329e
Ghezo22 <ochegoddy22@gmail.com>
2026-05-12 21:22:08 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("8768027310:AAG0kNJAEPKQkLKMMrOeeL31BCd9YE0-uDQ")
+TOKEN = os.environ.get("8768027310:AAFaCGr5pXexsT-tV5bG14nwwPVGVBPHfSg")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is working ✅")
