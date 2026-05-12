efa2a9b36d621cf26d01a5fa0c565721d0201d59
Ghezo22 <ochegoddy22@gmail.com>
2026-05-12 23:16:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-
-
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -7,10 +5,11 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.environ.get("8768027310:AAFaCGr5pXexsT-tV5bG14nwwPVGVBPHfSg")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working ✅")
+    await update.message.reply_text("Bot is alive ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
+print("BOT RUNNING...")
 app.run_polling()
