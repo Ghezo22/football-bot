bc63530898189ca1bbc1e3f3df84452d6ca7d75a
Ghezo22 <ochegoddy22@gmail.com>
2026-05-12 22:48:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
+
+
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.environ.get("8768027310:AAFaCGr5pXexsT-tV5bG14nwwPVGVBPHfSg")
 
+if not TOKEN:
+    raise Exception("BOT_TOKEN is missing in Railway variables")
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is alive 🚀")
 
 async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Commands:\n/start\n/help\n/ping")
-
-async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("PONG 🟢")
+    await update.message.reply_text("Use /start or send a message")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
@@ -20,7 +22,6 @@ app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help_cmd))
-app.add_handler(CommandHandler("ping", ping))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 app.run_polling()
